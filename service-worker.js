abceadf1309522de326bc8d9312ed58500584cd6
Github-Tools-Collection <132978204+Github-Tools-Collection@users.noreply.github.com>
2025-03-15 06:49:58 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,26 @@
+const cacheName = "youtube-pwa-cache-v1";
+const assets = [
+  "/index.html",
+  "/manifest.json",
+  "/main.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install Service Worker and Cache Files
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("youtube-cache").then((cache) => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/manifest.json",
-                "/icon-192x192.png",
-                "/icon-512x512.png"
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Serve Cached Files When Offline
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
