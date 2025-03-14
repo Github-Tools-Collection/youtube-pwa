59524d9e65e7e3481ebdf5cebb7ba802173a20e9
Github-Tools-Collection <132978204+Github-Tools-Collection@users.noreply.github.com>
2025-03-15 06:08:12 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("youtube-pwa").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icon-192x192.png",
-        "/icon-512x512.png"
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open("youtube-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/icon-192x192.png",
+                "/icon-512x512.png"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
